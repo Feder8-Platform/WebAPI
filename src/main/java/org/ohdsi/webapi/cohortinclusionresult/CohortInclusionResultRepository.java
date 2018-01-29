b0e9ae22a984127d8c1695e2f1d6c7382f181e3e
Sander Bylemans <sbylemans@gmail.com>
2018-01-29 16:37:12 +0100

    WebAPI import generation results (currently to the wrong schema -> imports to webapi instead of results)

---
+++ src/main/java/org/ohdsi/webapi/cohortinclusionresult/CohortInclusionResultRepository.java
@@ -0,0 +1,6 @@
+package org.ohdsi.webapi.cohortinclusionresult;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CohortInclusionResultRepository extends CrudRepository<CohortInclusionResultEntity, Long> {
+}
---
+++ src/main/java/org/ohdsi/webapi/cohortinclusionstats/CohortInclusionStatsRepository.java
@@ -0,0 +1,6 @@
+package org.ohdsi.webapi.cohortinclusionstats;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CohortInclusionStatsRepository extends CrudRepository<CohortInclusionStatsEntity, Long> {
+}
