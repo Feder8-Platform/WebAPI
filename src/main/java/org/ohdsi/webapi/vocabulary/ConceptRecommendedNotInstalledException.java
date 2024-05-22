ca3c8abb0dc1301669f73f53b399f393c8c2e4c5
silviajimeneznavarro-edence <silvia.jimeneznavarro@edence.health>
2024-05-22 14:57:41 +0200

    [AEKR-1989] typo in a file after big merge

--- src/main/java/org/ohdsi/webapi/vocabulary/ConceptRecommendedNotInstalledException.java
+++ src/main/java/org/ohdsi/webapi/vocabulary/ConceptRecommendedNotInstalledException.java
@@ -1,9 +1,5 @@
 /*
-<<<<<<<< HEAD:src/main/java/org/ohdsi/webapi/cache/CacheInfo.java
- * Copyright 2019 cknoll1.
-========
  * Copyright 2022 cknoll1.
->>>>>>>> refs/remotes/OHDSI_WebAPI/master-2.14:src/main/java/org/ohdsi/webapi/vocabulary/ConceptRecommendedNotInstalledException.java
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,22 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-<<<<<<<< HEAD:src/main/java/org/ohdsi/webapi/cache/CacheInfo.java
-package org.ohdsi.webapi.cache;
-========
 package org.ohdsi.webapi.vocabulary;
->>>>>>>> refs/remotes/OHDSI_WebAPI/master-2.14:src/main/java/org/ohdsi/webapi/vocabulary/ConceptRecommendedNotInstalledException.java
 
 /**
  *
  * @author cknoll1
  */
-<<<<<<<< HEAD:src/main/java/org/ohdsi/webapi/cache/CacheInfo.java
-public class CacheInfo {
-	public String cacheName;
-	public Long entries;
-========
 public class ConceptRecommendedNotInstalledException extends RuntimeException{
   
->>>>>>>> refs/remotes/OHDSI_WebAPI/master-2.14:src/main/java/org/ohdsi/webapi/vocabulary/ConceptRecommendedNotInstalledException.java
 }
