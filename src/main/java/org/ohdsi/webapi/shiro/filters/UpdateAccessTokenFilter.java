7c7f079433e597aa4bd4801e33050f83423c12eb
silviajimeneznavarro-edence <silvia.jimeneznavarro@edence.health>
2025-03-21 16:17:08 +0100

    [AEKR-1989] webapi is correctly storing the users and token works ok

--- src/main/java/org/ohdsi/webapi/shiro/filters/UpdateAccessTokenFilter.java
+++ src/main/java/org/ohdsi/webapi/shiro/filters/UpdateAccessTokenFilter.java
@@ -68,7 +68,7 @@ public class UpdateAccessTokenFilter extends AdviceFilter {
     Object principal = principals.getPrimaryPrincipal();
     
     if (principal instanceof Pac4jPrincipal) {
-      login = ((Pac4jPrincipal)principal).getProfile().getEmail();
+      login = ((Pac4jPrincipal)principal).getProfile().getUsername();
       name = ((Pac4jPrincipal)principal).getProfile().getDisplayName();
       
       /**
